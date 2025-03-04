95a945a6f8a24335390376084cd9e29f280c788e
CSC <77079770+christianschan41899@users.noreply.github.com>
2025-03-04 23:44:06 +0000

    Further code correction

--- Services/GetFeed.py
+++ Services/GetFeed.py
@@ -3,5 +3,5 @@ import datetime
 def get_feed():
     to_date = datetime.datetime.now()
     from_date = to_date - datetime.timedelta(days=1)
-    posts = API.get_posts(from_date, to_date)
+    posts = API.API().get_posts(from_date, to_date)
     return posts
